bbfbf275846a223cb0f0166ec4fb2e128c6f77a7
baldram <baldram+github@gmail.com>
2017-07-02 20:30:12 +0200

    Refactor VS1053 library class

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# VS1053
+# VS1053 library
 
 This is a library for the generic **VS1053 Codec Breakout**.
 A powerful MP3 / ACC / WMA decoder.
@@ -10,5 +10,5 @@ Designed specifically to work with the **Espressif ESP8266 and ESP32** boards.
 There are currently two methods to program the ESP boards: the ESP-IDF and the ESP8266/ESP32 arduino Core.
 The library was created to work with the **arduino Core**.
  
-The ESP8266 is the most popular Wi-Fi MCU (know also as ESP12, NodeMCU, WeMos, ...). 
+The ESP8266 is the most popular Wi-Fi MCU (known also as ESP12, NodeMCU, WeMos, ...). 
 But the library should also work with classic Arduino Uno board too.
---
+++ VS1053.h
@@ -0,0 +1,114 @@
+/**
+ * This is a driver library for VS1053 MP3 Codec Breakout
+ * (Ogg Vorbis / MP3 / AAC / WMA / FLAC / MIDI Audio Codec Chip).
+ * Adapted for Espressif ESP8266 and ESP32 boards.
+ *
+ * @authors baldram, edzelf, MagicCube, maniacbug
+ *
+ * Development log:
+ *  - 2011: based on VS1053 Arduino library originally written by Maniacbug,
+ *  - 2016: refactored by Ed Smallenburg (edzelf) and integrated into Esp-radio sketch,
+ *  - 2017: refactored to use as Platformio library by Marcin Szalomski (baldram[at]gmail.com)
+ */
+
+#ifndef VS1053_H
+#define VS1053_H
+
+#include <Arduino.h>
+#include <SPI.h>
+
+class VS1053 {
+private:
+    uint8_t cs_pin;                         // Pin where CS line is connected
+    uint8_t dcs_pin;                        // Pin where DCS line is connected
+    uint8_t dreq_pin;                       // Pin where DREQ line is connected
+    uint8_t curvol;                         // Current volume setting 0..100%
+    const uint8_t vs1053_chunk_size = 32;
+    // SCI Register
+    const uint8_t SCI_MODE = 0x0;
+    const uint8_t SCI_BASS = 0x2;
+    const uint8_t SCI_CLOCKF = 0x3;
+    const uint8_t SCI_AUDATA = 0x5;
+    const uint8_t SCI_WRAM = 0x6;
+    const uint8_t SCI_WRAMADDR = 0x7;
+    const uint8_t SCI_AIADDR = 0xA;
+    const uint8_t SCI_VOL = 0xB;
+    const uint8_t SCI_AICTRL0 = 0xC;
+    const uint8_t SCI_AICTRL1 = 0xD;
+    const uint8_t SCI_num_registers = 0xF;
+    // SCI_MODE bits
+    const uint8_t SM_SDINEW = 11;           // Bitnumber in SCI_MODE always on
+    const uint8_t SM_RESET = 2;             // Bitnumber in SCI_MODE soft reset
+    const uint8_t SM_CANCEL = 3;            // Bitnumber in SCI_MODE cancel song
+    const uint8_t SM_TESTS = 5;             // Bitnumber in SCI_MODE for tests
+    const uint8_t SM_LINE1 = 14;            // Bitnumber in SCI_MODE for Line input
+    SPISettings VS1053_SPI;                 // SPI settings for this slave
+    uint8_t endFillByte;                    // Byte to send when stopping song
+protected:
+    inline void await_data_request() const {
+        while (!digitalRead(dreq_pin)) {
+            yield();                        // Very short delay
+        }
+    }
+
+    inline void control_mode_on() const {
+        SPI.beginTransaction(VS1053_SPI);   // Prevent other SPI users
+        digitalWrite(dcs_pin, HIGH);        // Bring slave in control mode
+        digitalWrite(cs_pin, LOW);
+    }
+
+    inline void control_mode_off() const {
+        digitalWrite(cs_pin, HIGH);         // End control mode
+        SPI.endTransaction();               // Allow other SPI users
+    }
+
+    inline void data_mode_on() const {
+        SPI.beginTransaction(VS1053_SPI);   // Prevent other SPI users
+        digitalWrite(cs_pin, HIGH);         // Bring slave in data mode
+        digitalWrite(dcs_pin, LOW);
+    }
+
+    inline void data_mode_off() const {
+        digitalWrite(dcs_pin, HIGH);        // End data mode
+        SPI.endTransaction();               // Allow other SPI users
+    }
+
+    uint16_t read_register(uint8_t _reg) const;
+
+    void write_register(uint8_t _reg, uint16_t _value) const;
+
+    void sdi_send_buffer(uint8_t *data, size_t len);
+
+    void sdi_send_fillers(size_t length);
+
+    void wram_write(uint16_t address, uint16_t data);
+
+    uint16_t wram_read(uint16_t address);
+
+public:
+    // Constructor.  Only sets pin values.  Doesn't touch the chip.  Be sure to call begin()!
+    VS1053(uint8_t _cs_pin, uint8_t _dcs_pin, uint8_t _dreq_pin);
+
+    bool begin();                               // Begin operation.  Sets pins correctly,
+                                                // and prepares SPI bus.
+    void startSong();                           // Prepare to start playing. Call this each
+                                                // time a new song starts.
+    void playChunk(uint8_t *data, size_t len);  // Play a chunk of data.  Copies the data to
+                                                // the chip.  Blocks until complete.
+    void stopSong();                            // Finish playing a song. Call this after
+                                                // the last playChunk call.
+    void setVolume(uint8_t vol);                // Set the player volume.Level from 0-100,
+                                                // higher is louder.
+    void setTone(uint8_t *rtone);               // Set the player baas/treble, 4 nibbles for
+                                                // treble gain/freq and bass gain/freq
+    uint8_t getVolume();                        // Get the currenet volume setting.
+                                                // higher is louder.
+    void printDetails(const char *header);      // Print configuration details to serial output.
+    void softReset();                           // Do a soft reset
+    bool testComm(const char *header);          // Test communication with module
+    inline bool data_request() const {
+        return (digitalRead(dreq_pin) == HIGH);
+    }
+};
+
+#endif
